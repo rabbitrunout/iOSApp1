8fb747d6599b9977f2a8e60bab3ecb38a600b2fd
Irina Saf <irina.safronova0801@gmail.com>
2025-09-17 13:54:43 -0400

    Added icons

---
+++ OrderTImHortons/Views/OrderDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderDetailView.swift
+//  OrderTImHortons
+//
+//  Created by Irina Saf on 2025-09-17.
+//
+
+import SwiftUI
+
+struct OrderDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderDetailView()
+}
