9989fe341149ee5552c664b12251df6d9a01e32b
Irina Saf <irina.safronova0801@gmail.com>
2025-09-17 11:27:11 -0400

    Initial Commit

---
+++ OrderTImHortons/OrderTImHortonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderTImHortonsApp.swift
+//  OrderTImHortons
+//
+//  Created by Irina Saf on 2025-09-17.
+//
+
+import SwiftUI
+
+@main
+struct OrderTImHortonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
